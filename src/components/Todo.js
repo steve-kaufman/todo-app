b680a1df4ab4fc7a17bd403b5601a22297e1bc4d
Steven Kaufman <steveplaysguiola@gmail.com>
2020-02-16 16:27:18 -0600

    added functionality to check todo by default

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,17 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import '../styles/components/Todo.sass'
 
-export default function Todo ({ name, description }) {
+export default function Todo ({ name, description, isCompleted }) {
   return (
     <div data-testid='todo' className='todo'>
       <h3 data-testid='name'>{ name }</h3>
       <p data-testid='description'>{ description }</p>
-      <input data-testid='checkbox' type='checkbox'></input>
+      <input 
+        data-testid='checkbox' 
+        type='checkbox' 
+        defaultChecked={isCompleted} />
       <button data-testid='delete-button'>Delete</button>
     </div>
   )
 }
 
 Todo.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  isCompleted: PropTypes.bool.isRequired
 }
