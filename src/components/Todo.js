e19f5797ad0cc99ac2f7680c40b487ac4cdf8080
Steven Kaufman <steveplaysguiola@gmail.com>
2020-02-16 15:20:53 -0600

    added some basic styling for Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import '../styles/components/Todo.sass'
 
 export default function Todo ({ name, description }) {
   return (
-    <div data-testid='todo'>
+    <div data-testid='todo' className='todo'>
       <h3 data-testid='name'>{ name }</h3>
       <p data-testid='description'>{ description }</p>
       <input data-testid='checkbox' type='checkbox'></input>
