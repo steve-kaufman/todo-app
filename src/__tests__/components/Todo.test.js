e52cfb0ecf38b1096066b259103862c609cee79c
Steven Kaufman <steveplaysguiola@gmail.com>
2020-02-27 16:42:14 -0600

    added tests for delete and toggle on Todo

--- src/__tests__/components/Todo.test.js
+++ src/__tests__/components/Todo.test.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, getByTestId } from '@testing-library/react'
 import Todo from '../../components/Todo'
 
 
 describe('Todo', () => {
-  let todo = null
+  let todo, toggleTodo, deleteTodo
+
+  const id = 2
 
   beforeEach(() => {
+    toggleTodo = deleteTodo = jest.fn()
     todo = render(<Todo 
-      id={2}
+      id={id}
       name='foo' 
       description='bar' 
       isCompleted={false} 
       isLast={false}
-      toggleTodo={jest.fn()}
-      deleteTodo={jest.fn()}
+      toggleTodo={toggleTodo}
+      deleteTodo={deleteTodo}
     />)
   })
 
@@ -47,24 +50,46 @@ describe('Todo', () => {
     expect(description).toHaveTextContent(/bar/i)
   })
 
-  it('has a checkbox', () => {
-    // arrange
-    const { queryByTestId } = todo
-    // act
-    const checkbox = queryByTestId('checkbox')
-    // assert
-    expect(checkbox).not.toBeNull()
-    expect(checkbox.tagName).toBe('INPUT')
-    expect(checkbox.type).toBe('checkbox')
+  describe('Checkbox', () => {
+    it('exists', () => {
+      // arrange
+      const { queryByTestId } = todo
+      // act
+      const checkbox = queryByTestId('checkbox')
+      // assert
+      expect(checkbox).not.toBeNull()
+      expect(checkbox.tagName).toBe('INPUT')
+      expect(checkbox.type).toBe('checkbox')
+    })
+    it('calls toggleTodo when clicked', () => {
+      // arrange
+      const { getByTestId } = todo
+      // act
+      getByTestId('checkbox').click()
+      // assert
+      expect(toggleTodo).toHaveBeenCalledTimes(1)
+      expect(toggleTodo).toHaveBeenCalledWith(id)
+    })
   })
 
-  it('has a delete button', () => {
-    // arrange
-    const { queryByTestId } = todo
-    // act
-    const deleteButton = queryByTestId('delete-button')
-    // assert
-    expect(deleteButton).not.toBeNull()
-    expect(deleteButton.tagName).toBe('BUTTON')
+  describe('Delete Button', () => {
+    it('exists', () => {
+      // arrange
+      const { queryByTestId } = todo
+      // act
+      const deleteButton = queryByTestId('delete-button')
+      // assert
+      expect(deleteButton).not.toBeNull()
+      expect(deleteButton.tagName).toBe('BUTTON')
+    })
+    it('calls deleteTodo when clicked', () => {
+      // arrange
+      const { getByTestId } = todo
+      // act
+      getByTestId('delete-button').click()
+      // assert
+      expect(toggleTodo).toHaveBeenCalledTimes(1)
+      expect(toggleTodo).toHaveBeenCalledWith(id)
+    })
   })
 })
